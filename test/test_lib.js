80c50c5a907bd732c4c4232b9a4cdc81f23cc586
Chandan Kumar <22887527+cksharma11@users.noreply.github.com>
2018-11-21 18:42:18 +0530

    Migrate tests
    1. Add more specific comments
    2. Add Test for empty array

--- test/test_lib.js
+++ test/test_lib.js
@@ -6,74 +6,68 @@ const {
 } = require("../src/lib.js");
 
 describe('map', function(){
-  it('should square given list of numbers', function(){
-    const square = function(number){
-      return number * number;
-    }
+  const square = function(number){
+    return number * number;
+  }
 
-    deepEqual(map(square, [1,2,3]),[1,4,9]);
-    deepEqual(map(square, [1]),[1]);
-    deepEqual(map(square, [1,7,3]),[1,49,9]);
-    deepEqual(map(square, [-1,-2,-3]),[1,4,9]);
+  const increment = function(number){
+    return number + 1;
+  }
+
+  it('should return empty array when mapped with empty array', function(){
+    deepEqual(map(square, []),[]);
   });
 
-  it('should increment given set of values by one', function(){
-    const increment = function(number){
-      return number + 1;
-    }
-  
-    deepEqual(map(increment, [1,2,3]),[2,3,4]);
+  it('should return one element when mapped with array of one element', function(){
     deepEqual(map(increment, [1]),[2]);
-    deepEqual(map(increment, [1,7,3]),[2,8,4]);
-    deepEqual(map(increment, [-1,-2,-3]),[0,-1,-2]);
+  });
+
+  it('should map more then one element', function(){
+    deepEqual(map(square,[1,2,3]), [1,4,9]);
+    deepEqual(map(increment,[1,2,3,4]), [2,3,4,5]);
   });
 });
 
 describe('filter', function(){
-  it('should filter even numbers', function(){
-    const isEven = function(evenCandidate){
-      return evenCandidate % 2 == 0;
-    }
+  const isEven = function(evenCandidate){
+    return evenCandidate % 2 == 0;
+  }
 
-    deepEqual(filter(isEven, [1,2,3]),[2]);
-    deepEqual(filter(isEven, [1]),[]);
-    deepEqual(filter(isEven, [1,7,3]),[]);
-    deepEqual(filter(isEven, [-1,-2,-3]),[-2]);
+  const isOdd = function(oddCandidate){
+    return oddCandidate % 2 != 0;
+  }
+
+  it('should return empty array when has empty array as an input', function(){
+    deepEqual(filter(isEven, []),[]);
   });
 
-  it('should filter odd numbers', function(){
-    const isOdd = function(oddCandidate){
-      return oddCandidate % 2 != 0;
-    }
-    deepEqual(filter(isOdd, [1,2,3]),[1,3]);
-    deepEqual(filter(isOdd, [1]),[1]);
-    deepEqual(filter(isOdd, [1,7,3]),[1,7,3]);
-    deepEqual(filter(isOdd, [-1,-2,-3]),[-1,-3]);
+  it('should return all alements when function returns true for all inputes', function(){
+    deepEqual(filter(isOdd, [1,3,5]),[1,3,5]);
+  });
+
+  it('should return empty array when function return false for all inputes', function(){
+    deepEqual(filter(isEven, [1,3,5]),[]);
   });
 });
 
 describe('reduce', function(){
-  it('should sum the given set of numbers', function(){
-    const sum = function(number1, number2){
-      return number1 + number2;
-    }
-    deepEqual(reduce(sum, [1,2,3]),6);
-    deepEqual(reduce(sum, [1]),1);
-    deepEqual(reduce(sum, [1,7,3]),11);
-    deepEqual(reduce(sum, [-1,-2,-3]),-6);
-    deepEqual(reduce(sum, [5,20], 10),35);
+  const sum = function(number1, number2){
+    return number1 + number2;
+  }
 
+  const multiply = function(number1, number2){
+    return number1 * number2;
+  }
+
+  it('should return undefined when input is empty array without initial value', function(){
+    deepEqual(reduce(sum, []),undefined);
   });
 
-  it('should give multiplication of given set of numbers ', function(){
-    const multiply = function(number1, number2){
-      return number1 * number2;
-    }
+  it('should return initial value when input is empty array with initial value ', function(){
+    deepEqual(reduce(multiply, [], 5),5);
+  });
 
-    deepEqual(reduce(multiply, [1,2,3]),6);
-    deepEqual(reduce(multiply, [1]),1);
-    deepEqual(reduce(multiply, [1,7,3]),21);
-    deepEqual(reduce(multiply, [-1,-2,-3]),-6);
-    deepEqual(reduce(multiply, [1,2], 2),4);
+  it('should return result when initial value and array with many elements', function(){
+    deepEqual(reduce(sum, [1,2,3],4),10);
   });
 });
